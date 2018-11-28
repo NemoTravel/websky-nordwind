ce8f7ce5f68046bc159d7ffca4ec9fe8d99f9197
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2018-11-28 18:48:27 +0400

    #38351 add nw-seat and nw-meal components to index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ require('./screens/add-services/AddServicesScreenController');
 require('./screens/search-order/search-order');
 require('./components/nordwind-es/nordwind-es');
 require('./components/nordwind-meal/nordwind-meal');
-require('./directives/switchInsurance');
 require('./components/nw-meal/nw-meal');
+require('./components/nw-seat/nw-seat');
+require('./components/nw-insurance/nw-insurance');
 require('./filters/toCity');
 
 
