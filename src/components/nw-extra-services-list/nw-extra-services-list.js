c842509fd9116b692a0de440587669f0f3fc7402
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2019-01-09 16:05:20 +0400

    38351 "Мой билет" - новая стилизация и верстка
    add switchInsurance function (from nw-insurance component) that call if only one passenger in order

--- src/components/nw-extra-services-list/nw-extra-services-list.js
+++ src/components/nw-extra-services-list/nw-extra-services-list.js
@@ -13,11 +13,53 @@ app.component('nwExtraServicesList', {
 });
 
 
-app.controller('extraServicesListController', ['utils', extraServicesListController]);
+app.controller('extraServicesListController', ['utils', 'backend', extraServicesListController]);
 
-function extraServicesListController(utils) {
+function extraServicesListController(utils, backend) {
 
     const vm = this;
     vm.isCommonService = utils.isCommonServiceCode;
+    vm.switchInsurance = switchInsurance;
+
+    vm.orderInfo = backend.getOrderInfo();
+
+    // не нужно показывать попап, по клику на страховку, если
+    // в заказе только один пассажир, просто вызываем modifyExtraServices
+    const insuranceES = _.findWhere(vm.es, {'code': 'insurance'});
+    vm.isSingleItemInInsurance = insuranceES && insuranceES.items.length === 1;
+
+
+    function switchInsurance() {
+        if (!vm.locked) {
+            insuranceES.active = !insuranceES.active;
+            if (insuranceES.active) {
+                if (insuranceES.items.length === 1) {
+                    backend.modifyExtraService(
+                        getInsuranceSubmitParams(insuranceES.items[0])
+                    );
+                }
+            } else {
+                backend.removeExtraService({
+                    code: insuranceES.onlineMode ? "insuranceOnline" : "insurance"
+                });
+            }
+        }
+    }
+
+    function getInsuranceSubmitParams(item, passenger_id) {
+        var params = {
+            code: "insurance",
+            ins: item.ins,
+            tins: item.tins
+        };
+        if (passenger_id) {
+            params.passenger_id = passenger_id;
+        }
+        if (insuranceES.onlineMode) {
+            params.code = "insuranceOnline";
+            params.productCode = item.productCode;
+        }
+        return params;
+    }
 
 }
