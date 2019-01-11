487a9adec1503da47f89835cfc8fd101dbfd1bf1
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2019-01-11 18:37:46 +0400

    38351 "Мой билет" - новая стилизация и верстка beautify

--- src/components/nw-meal/nw-meal.js
+++ src/components/nw-meal/nw-meal.js
@@ -1,189 +1,200 @@
-var app = angular.module('app');
+var app = angular.module("app");
 
-app.component('nwMeal', {
-    templateUrl: 'components/nw-meal/nw-meal.html',
-    controller: ['$scope', '$element', 'backend', 'utils', MealController],
-    controllerAs: 'vm',
-    bindings: {
-        service: '=service',
-        locked: '=locked',
-        closePopup: '&',
-    }
+app.component("nwMeal", {
+	templateUrl: "components/nw-meal/nw-meal.html",
+	controller: ["$scope", "$element", "backend", "utils", MealController],
+	controllerAs: "vm",
+	bindings: {
+		service: "=service",
+		locked: "=locked",
+		closePopup: "&"
+	}
 });
 
 function MealController($scope, $element, backend, utils) {
+	var vm = this,
+		passengersTableContainer = $element.find("div.passengersTableContainer");
 
-    var vm = this,
-        passengersTableContainer = $element.find('div.passengersTableContainer');
+	vm.switchService = switchService;
+	vm.selectFlightPassenger = selectFlightPassenger;
+	vm.selectFirstAvailablePassengerFlight = selectFirstAvailablePassengerFlight;
+	vm.mealCountChangeHandler = mealCountChangeHandler;
+	vm.removePassengerFlightMeal = removePassengerFlightMeal;
+	vm.getSelectedPassengerFlightMeal = getSelectedPassengerFlightMeal;
+	vm.scrollToStart = scrollToStart;
+	vm.scrollToEnd = scrollToEnd;
+	vm.hasAlias = backend.hasAlias;
+	vm.getAvailablePassengersCount = utils.getAvailablePassengersCount;
+	vm.checkServiceRemovalProhibited = backend.checkServiceRemovalProhibited;
+	vm.closePopup = vm.closePopup();
 
-    vm.switchService = switchService;
-    vm.selectFlightPassenger = selectFlightPassenger;
-    vm.selectFirstAvailablePassengerFlight = selectFirstAvailablePassengerFlight;
-    vm.mealCountChangeHandler = mealCountChangeHandler;
-    vm.removePassengerFlightMeal = removePassengerFlightMeal;
-    vm.getSelectedPassengerFlightMeal = getSelectedPassengerFlightMeal;
-    vm.scrollToStart = scrollToStart;
-    vm.scrollToEnd = scrollToEnd;
-    vm.hasAlias = backend.hasAlias;
-    vm.getAvailablePassengersCount = utils.getAvailablePassengersCount;
-    vm.checkServiceRemovalProhibited = backend.checkServiceRemovalProhibited;
-    vm.closePopup = vm.closePopup();
+	vm.canScrollRight = true;
+	vm.canScrollLeft = false;
 
-    vm.canScrollRight = true;
-    vm.canScrollLeft = false;
+	vm.selectedFlight = 0;
+	vm.selectedPassenger = 0;
 
-    vm.selectedFlight = 0;
-    vm.selectedPassenger = 0;
+	if (backend.getParam("site.externalStorageBaseUrl")) {
+		vm.mealImagesPath =
+			backend.getParam("site.externalStorageBaseUrl") + "/img/content/meal";
+	} else {
+		vm.mealImagesPath = "./themes/websky/assets/static/img/content/meal";
+	}
 
-    if (backend.getParam('site.externalStorageBaseUrl')) {
-        vm.mealImagesPath = backend.getParam('site.externalStorageBaseUrl') + '/img/content/meal';
-    } else {
-        vm.mealImagesPath = './themes/websky/assets/static/img/content/meal';
-    }
+	backend.addOrderInfoListener(
+		function(orderInfo) {
+			vm.orderInfo = orderInfo;
+		},
+		false,
+		true
+	);
 
-    backend.addOrderInfoListener(function (orderInfo) {
-        vm.orderInfo = orderInfo;
-    }, false, true);
+	$scope.$watch("vm.service", function() {
+		updateMealMenu();
+	});
 
-    $scope.$watch('vm.service', function () {
-        updateMealMenu();
-    });
+	passengersTableContainer.on("scroll", mobileTableScrollHandler);
 
-    passengersTableContainer.on('scroll', mobileTableScrollHandler);
+	if (vm.service.active) {
+		updateMealMenu();
+	}
 
-    if (vm.service.active) {
-        updateMealMenu();
-    }
+	function switchService() {
+		if (!vm.locked && !backend.checkServiceRemovalProhibited("meal")) {
+			vm.service.active = !vm.service.active;
+			if (vm.service.active) {
+				selectFirstAvailablePassengerFlight();
+			} else {
+				backend.removeExtraService({
+					code: "meal"
+				});
+			}
+			vm.closePopup();
+		}
+	}
 
-    function switchService() {
-        if (!vm.locked && !backend.checkServiceRemovalProhibited('meal')) {
-            vm.service.active = !vm.service.active;
-            if (vm.service.active) {
-                selectFirstAvailablePassengerFlight();
-            } else {
-                backend.removeExtraService({
-                    code: 'meal'
-                });
-            }
-            vm.closePopup();
-        }
-    }
+	function selectFlightPassenger(flightNum, passengerNum) {
+		vm.selectedFlight = flightNum;
+		vm.selectedPassenger = passengerNum;
+		updateMealMenu();
+	}
 
-    function selectFlightPassenger(flightNum, passengerNum) {
-        vm.selectedFlight = flightNum;
-        vm.selectedPassenger = passengerNum;
-        updateMealMenu();
-    }
+	function selectFirstAvailablePassengerFlight() {
+		vm.selectedFlight = getFirstAvailableFlightNum();
+		vm.selectedPassenger = getFirstAvailablePassengerNum(vm.selectedFlight);
+		updateMealMenu();
+	}
 
-    function selectFirstAvailablePassengerFlight() {
-        vm.selectedFlight = getFirstAvailableFlightNum();
-        vm.selectedPassenger = getFirstAvailablePassengerNum(vm.selectedFlight);
-        updateMealMenu();
-    }
+	function updateMealMenu() {
+		vm.mealMenu =
+			vm.service.itemsByPassengerSegments[vm.selectedPassenger][
+				vm.selectedFlight
+			];
+		vm.mealMenuSubgroup = false;
+	}
 
-    function updateMealMenu() {
-        vm.mealMenu = vm.service.itemsByPassengerSegments[vm.selectedPassenger][vm.selectedFlight];
-        vm.mealMenuSubgroup = false;
-    }
+	function mealCountChangeHandler(subgroupNum, mealItem, delta) {
+		if (!vm.locked) {
+			backend.modifyExtraService({
+				code: "meal",
+				passenger_id: vm.orderInfo.passengers[vm.selectedPassenger].id,
+				segment_id: vm.orderInfo.plainFlights[vm.selectedFlight].id,
+				subgroup: vm.service.subgroups[subgroupNum],
+				amount: (mealItem.alreadySelectedCount || 0) + delta,
+				rfisc: mealItem.rfisc
+			});
+		}
+	}
 
-    function mealCountChangeHandler(subgroupNum, mealItem, delta) {
-        if (!vm.locked) {
-            backend.modifyExtraService({
-                code: 'meal',
-                passenger_id: vm.orderInfo.passengers[vm.selectedPassenger].id,
-                segment_id: vm.orderInfo.plainFlights[vm.selectedFlight].id,
-                subgroup: vm.service.subgroups[subgroupNum],
-                amount: (mealItem.alreadySelectedCount || 0) + delta,
-                rfisc: mealItem.rfisc
-            });
-        }
-    }
+	function removePassengerFlightMeal(passengerNum, flightNum) {
+		if (
+			!vm.locked &&
+			!backend.checkServiceRemovalProhibited("meal", passengerNum, flightNum)
+		) {
+			backend.removeExtraService({
+				code: "meal",
+				passenger_id: vm.orderInfo.passengers[passengerNum].id,
+				segment_id: vm.orderInfo.plainFlights[flightNum].id
+			});
+		}
+	}
 
-    function removePassengerFlightMeal(passengerNum, flightNum) {
-        if (!vm.locked && !backend.checkServiceRemovalProhibited('meal', passengerNum, flightNum)) {
-            backend.removeExtraService({
-                code: 'meal',
-                passenger_id: vm.orderInfo.passengers[passengerNum].id,
-                segment_id: vm.orderInfo.plainFlights[flightNum].id
-            });
-        }
-    }
+	function getSelectedPassengerFlightMeal(passengerNum, flightNum) {
+		var count = 0,
+			cost = 0,
+			currency;
 
-    function getSelectedPassengerFlightMeal(passengerNum, flightNum) {
+		if (
+			vm.orderInfo.groupedEditableExtraServices &&
+			vm.orderInfo.groupedEditableExtraServices.meal &&
+			vm.orderInfo.groupedEditableExtraServices.meal[passengerNum] &&
+			vm.orderInfo.groupedEditableExtraServices.meal[passengerNum][flightNum] &&
+			vm.orderInfo.groupedEditableExtraServices.meal[passengerNum][flightNum]
+				.length
+		) {
+			vm.orderInfo.groupedEditableExtraServices.meal[passengerNum][
+				flightNum
+			].forEach(function(item) {
+				if (item.amount) {
+					count += item.amount;
+				}
+				if (item.totalPrice) {
+					cost = Big(cost)
+						.plus(item.totalPrice)
+						.toFixed(2);
+					currency = item.currency;
+				}
+			});
+		}
 
-        var count = 0,
-            cost = 0,
-            currency;
+		if (count) {
+			return {
+				count: count,
+				cost: cost,
+				currency: currency
+			};
+		} else {
+			return false;
+		}
+	}
 
-        if (
-            vm.orderInfo.groupedEditableExtraServices &&
-            vm.orderInfo.groupedEditableExtraServices.meal &&
-            vm.orderInfo.groupedEditableExtraServices.meal[passengerNum] &&
-            vm.orderInfo.groupedEditableExtraServices.meal[passengerNum][flightNum] &&
-            vm.orderInfo.groupedEditableExtraServices.meal[passengerNum][flightNum].length
-        ) {
-            vm.orderInfo.groupedEditableExtraServices.meal[passengerNum][flightNum].forEach(function (item) {
-                if (item.amount) {
-                    count += item.amount;
-                }
-                if (item.totalPrice) {
-                    cost = Big(cost).plus(item.totalPrice).toFixed(2);
-                    currency = item.currency;
-                }
-            });
-        }
+	function scrollToStart() {
+		passengersTableContainer.scrollTo(0);
+	}
 
-        if (count) {
-            return {
-                count: count,
-                cost: cost,
-                currency: currency
-            };
-        } else {
-            return false;
-        }
+	function scrollToEnd() {
+		passengersTableContainer.scrollTo(passengersTableContainer[0].scrollWidth);
+	}
 
-    }
+	function mobileTableScrollHandler() {
+		var scrollLeft = passengersTableContainer[0].scrollLeft,
+			scrollWidth = passengersTableContainer[0].scrollWidth,
+			clientWidth = passengersTableContainer[0].clientWidth,
+			scrollRight = scrollWidth - clientWidth - scrollLeft;
+		vm.canScrollRight = scrollRight !== 0;
+		vm.canScrollLeft = scrollLeft !== 0;
+		$scope.$apply();
+	}
 
-    function scrollToStart() {
-        passengersTableContainer.scrollTo(0);
-    }
+	function getFirstAvailableFlightNum() {
+		var i;
+		for (i = 0; i < vm.orderInfo.plainFlights.length; i++) {
+			if (vm.service.availableBySegments[i]) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
-    function scrollToEnd() {
-        passengersTableContainer.scrollTo(passengersTableContainer[0].scrollWidth);
-    }
-
-    function mobileTableScrollHandler() {
-        var scrollLeft = passengersTableContainer[0].scrollLeft,
-            scrollWidth = passengersTableContainer[0].scrollWidth,
-            clientWidth = passengersTableContainer[0].clientWidth,
-            scrollRight = scrollWidth - clientWidth - scrollLeft;
-        vm.canScrollRight = (scrollRight !== 0);
-        vm.canScrollLeft = (scrollLeft !== 0);
-        $scope.$apply();
-    }
-
-    function getFirstAvailableFlightNum() {
-        var i;
-        for (i = 0; i < vm.orderInfo.plainFlights.length; i++) {
-            if (vm.service.availableBySegments[i]) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    function getFirstAvailablePassengerNum(flightNum) {
-        var i;
-        if (vm.service.availableByPassengerSegments) {
-            for (i = 0; i < vm.orderInfo.passengers.length; i++) {
-                if (vm.service.availableByPassengerSegments[i][flightNum]) {
-                    return i;
-                }
-            }
-        }
-        return -1;
-    }
-
+	function getFirstAvailablePassengerNum(flightNum) {
+		var i;
+		if (vm.service.availableByPassengerSegments) {
+			for (i = 0; i < vm.orderInfo.passengers.length; i++) {
+				if (vm.service.availableByPassengerSegments[i][flightNum]) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 }
-
