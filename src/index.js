9e20932f2bf2a7b736305c5ac12f23bde1d1a973
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2019-01-11 18:33:07 +0400

    38351 "Мой билет" - новая стилизация и верстка
    add nw-insurance to index.js and beautify

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
-require('./screens/add-services/AddServicesScreenController');
-require('./screens/search-order/search-order');
-require('./directives/replaceWith');
-require('./components/nw-es-popup/nw-es-popup');
-require('./components/order-with-es/order-with-es');
-require('./components/nw-extra-services-list/nw-extra-services-list');
-require('./components/nw-meal/nw-meal');
-require('./components/nw-seat/nw-seat');
-require('./filters/toCity');
+require("./screens/add-services/AddServicesScreenController");
+require("./screens/search-order/search-order");
+require("./directives/replaceWith");
+require("./components/nw-es-popup/nw-es-popup");
+require("./components/order-with-es/order-with-es");
+require("./components/nw-extra-services-list/nw-extra-services-list");
+require("./components/nw-meal/nw-meal");
+require("./components/nw-seat/nw-seat");
+require("./components/nw-insurance/nw-insurance");
+require("./filters/toCity");
 
 // TODO: remove it!!
-angular.module('app').config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.debugInfoEnabled(true);
-}]);
+angular.module("app").config([
+	"$compileProvider",
+	function($compileProvider) {
+		$compileProvider.debugInfoEnabled(true);
+	}
+]);
 
-angular.module('app').run(['$rootScope', 'redirect', 'backend', function ($rootScope, redirect, backend) {
-    $rootScope.$on('$locationChangeStart', function (e) {
+angular.module("app").run([
+	"$rootScope",
+	"redirect",
+	"backend",
+	function($rootScope, redirect, backend) {
+		$rootScope.$on("$locationChangeStart", function(e) {
+			var href = location.href.split("#")[1];
 
-        var href = location.href.split('#')[1];
+			href = href.split("/");
 
-        href = href.split('/');
+			var route = href[1],
+				pnr = href[2],
+				lastName = href[3];
 
-        var route = href[1],
-            pnr = href[2],
-            lastName = href[3];
+			if (route === "add-services" && pnr && lastName) {
+				lastName = decodeURIComponent(lastName);
 
-
-        if (route === 'add-services' && pnr && lastName) {
-            lastName = decodeURIComponent(lastName);
-
-            redirect.goToSearchOrder(pnr, lastName);
-        }
-
-    })
-}]);
+				redirect.goToSearchOrder(pnr, lastName);
+			}
+		});
+	}
+]);
