34a2bbd2b036575587da4bf58b6a4535cea5b0b8
LitvinenkoEvgeny <yourbisnesspartner@gmail.com>
2019-01-14 13:21:11 +0400

    38351 "Мой билет" - новая стилизация и верстка beautify nw-es-popup

--- src/components/nw-es-popup/nw-es-popup.js
+++ src/components/nw-es-popup/nw-es-popup.js
@@ -1,54 +1,58 @@
-angular.module('app').component('nwEsPopup', {
-    templateUrl: 'components/nw-es-popup/nw-es-popup.html',
-    controller: 'nwMealPopupController',
-    bindToController: true,
-    controllerAs: 'vm',
-    bindings: {
-        locked: '=',
-        esCode: '<',
-        es: '='
-    },
-    transclude: {
-        'extra-service': 'wrap',
-    },
+angular.module("app").component("nwEsPopup", {
+	templateUrl: "components/nw-es-popup/nw-es-popup.html",
+	controller: "nwMealPopupController",
+	bindToController: true,
+	controllerAs: "vm",
+	bindings: {
+		locked: "=",
+		esCode: "<",
+		es: "="
+	},
+	transclude: {
+		"extra-service": "wrap"
+	}
 });
 
-
-angular.module('app').controller('nwMealPopupController', ['$scope', 'backend', 'fancyboxTools', nwMealPopupController]);
+angular
+	.module("app")
+	.controller("nwMealPopupController", [
+		"$scope",
+		"backend",
+		"fancyboxTools",
+		nwMealPopupController
+	]);
 
 function nwMealPopupController($scope, backend, fancyboxTools) {
+	var vm = this;
+	var submitCallback = false;
+	vm.isSingleItemInInsurance = false;
 
-    var vm = this;
-    var submitCallback = false;
-    vm.isSingleItemInInsurance = false;
+	vm.close = close;
+	vm.submit = submit;
 
-    vm.close = close;
-    vm.submit = submit;
+	vm.popupId = "#nw-" + vm.esCode + "-popup";
 
-    vm.popupId = '#nw-' + vm.esCode + '-popup';
+	fancyboxTools.setOpenListener("nw-" + vm.esCode + "-popup", function(
+		link,
+		params
+	) {
+		var popup = angular.element(vm.popupId);
+		var popupWrapper = popup.parents(".fancybox-wrap");
 
+		// нужно прописать класс, чтобы проставить ширину попапа
+		if (!popupWrapper.hasClass("extra-service__popup")) {
+			popupWrapper.addClass("extra-service__popup");
+		}
+	});
 
+	function submit() {
+		if (submitCallback) {
+			submitCallback(true);
+			close();
+		}
+	}
 
-    fancyboxTools.setOpenListener('nw-' + vm.esCode + '-popup', function (link, params) {
-        var popup = angular.element(vm.popupId);
-        var popupWrapper = popup.parents('.fancybox-wrap');
-
-
-        // нужно прописать класс, чтобы проставить ширину попапа
-        if (!popupWrapper.hasClass('extra-service__popup')) {
-            popupWrapper.addClass('extra-service__popup');
-        }
-
-    });
-
-    function submit() {
-        if (submitCallback) {
-            submitCallback(true);
-            close();
-        }
-    }
-
-    function close() {
-        jQuery.fancybox.close();
-    }
+	function close() {
+		jQuery.fancybox.close();
+	}
 }
